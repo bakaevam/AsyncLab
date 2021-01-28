5ed13259f95fc40eb0f2758d8720a9c9d28d3f84
Maria Bakaeva <mary396969@gmail.com>
2021-01-28 12:26:05 +0300

    version 1

--- app/src/main/java/ru/anfilek/asyncLab/MyHandlerThread.kt
+++ app/src/main/java/ru/anfilek/asyncLab/MyHandlerThread.kt
@@ -1,17 +1,18 @@
 package ru.anfilek.asyncLab
 
 import android.os.Handler
+import android.os.Looper
 import android.os.Message
 import android.util.Log
 import java.util.*
 
-class MyHandlerThread : android.os.HandlerThread(TAG), Handler.Callback {
+class MyHandlerThread: android.os.HandlerThread(TAG), Handler.Callback {
 
     companion object {
         private const val TAG = "MyHandlerThread"
     }
 
-    lateinit var handler: Handler
+    private var handler: Handler? = null
         private set
 
     override fun onLooperPrepared() {
@@ -19,9 +20,8 @@ class MyHandlerThread : android.os.HandlerThread(TAG), Handler.Callback {
     }
 
     fun post() {
-        handler.sendEmptyMessage(Random(10).nextInt())
+        handler?.sendEmptyMessage(Random(10).nextInt())
     }
-
 
     override fun handleMessage(msg: Message): Boolean {
         Log.d(TAG, "handler message: ${msg.what}: Thread: ${Thread.currentThread().name}")
